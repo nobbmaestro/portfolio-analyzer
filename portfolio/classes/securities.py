a140a2c89867ac7be781ef51f3185fe2f2a51b51
Norbert Batiuk <norbert@batiuk.se>
2022-01-08 13:13:54 +0100

    added get_security_price method for Security class

--- portfolio/classes/securities.py
+++ portfolio/classes/securities.py
@@ -29,19 +29,99 @@ class Security(object):
             self.security = security
             try:
                 self.name, self.full_name, self.security_type, self.isin, self.symbol, self.country, self.currency  = self.get_basic_info()
-                self.latest_price = self.get_security_price()
-                
+
             except RuntimeError as e:
                 raise(RuntimeError(e))
 
     def __repr__(self):
-        pass
+        return self.name
 
-    def get_security_price(self):
-        pass
 
+    def get_security_price(self, from_date = datetime.date.today(), to_date = None):
+        """Return a dictionary of the historical price for the given security. If no args given, return latest close price.
+
+        Args:
+            from_date (datetime.date, optional): user defined from date. Defaults to datetime.date.today().
+            to_date (datetime.date, optional): user defined to date. Defaults to None.
+
+        Raises:
+            RuntimeError: security type not supported
+
+        Returns:
+            dict: with key datetime.date and value of the price as int
+        """
+        def format_security_price(price_data, only_last_row, auto_fill = False):
+            price_data = price_data.Close.to_dict()
+            formated_price_data = {}
+
+            if only_last_row:
+                last_row_index = list(price_data.keys())[-1]
+                formated_price_data[datetime.date(year=last_row_index.year, month=last_row_index.month, day=last_row_index.day)] = price_data[last_row_index]
+
+            else:
+                for row_index in price_data.keys():
+                    formated_price_data[datetime.date(year=row_index.year, month=row_index.month, day=row_index.day)] = price_data[row_index]
+                    if auto_fill and (to_date - from_date + datetime.timedelta(days=1)) != datetime.timedelta(days=len(list(price_data.keys()))):
+                        pass
+
+            return formated_price_data
+            
+        if to_date == None:
+            to_date = from_date
+            from_date = from_date - datetime.timedelta(days=1)
+    
+        if self.security_type == 'stock':
+            price_data = (
+                eval(self.get_historical_data_query_investpy(
+                    security_type= self.security_type, 
+                    security=self.symbol, 
+                    country=self.country,
+                    from_date=from_date, 
+                    to_date=to_date
+                    )
+                )
+            )
+        elif self.security_type == 'fund':
+            price_data = (
+                eval(self.get_historical_data_query_investpy(
+                    security_type= self.security_type, 
+                    security=self.name, 
+                    country=self.country,
+                    from_date=from_date, 
+                    to_date=to_date
+                    )
+                )
+            )
+        elif self.security_type == 'currency_cross':
+            price_data = (
+                eval(self.get_historical_data_query_investpy(
+                    security_type= self.security_type, 
+                    security=self.name, 
+                    country= None,
+                    from_date=from_date, 
+                    to_date=to_date, 
+                    )
+                )
+            )
+        else:
+            raise RuntimeError('security type not supported')
+
+        if (to_date - from_date) ==  datetime.timedelta(days=1) and len(price_data.index) > 1:
+            return format_security_price(price_data, True)
+                
+        else:
+            return format_security_price(price_data, False)
 
     def get_basic_info(self):
+        """Returns basic information about the security
+
+            Raises:
+                RuntimeError: several matched found
+                RuntimeError: no results were found for the given security'
+
+            Returns:
+                dict: containing name, full name, security type, isin, symbol, country and currency of the given security
+        """
         def format_basic_info(security_type, basic_info):
             try:
                 name = basic_info.to_dict()['name'][0]
@@ -79,7 +159,7 @@ class Security(object):
 
             return name, full_name, security_type, isin, symbol, country, currency
             
-        
+            
         if self.SECURITY_IDENTIFIED:
             return {
                 'name'          : self.name,
@@ -107,11 +187,7 @@ class Security(object):
                     else:
                         raise RuntimeError(e)
             
-            raise RuntimeError('no results were found for the introduced security')
-
-        
-
-
+            raise RuntimeError('no results were found for the given security')
 
     def get_search_security_query_investpy(self, security_type, security):
         """Prepares the investpy query, output ready to be used with eval().
@@ -145,4 +221,73 @@ class Security(object):
         else:
             return q_search_security[security_type.lower()].format(security= security.upper())
 
-   
+    def get_historical_data_query_investpy(self, security_type, security, country, from_date = datetime.date.today() - datetime.timedelta(days=1), to_date = datetime.date.today()):
+        """Prepares the investpy query, output ready to be used with eval().
+
+        Args:
+            security_type (str): Type for security. 
+                Available types: 'stock', 'fund' or 'currency_cross'.
+                Defaults to None.
+
+            security (str): 
+                If 'stock' or 'fund': ISIN of the security . 
+                Defaults to None.
+
+            from_date (datetime.date, optional): 
+                From date of the historical data. 
+                Defaults to datetime.date.today() - datetime.timedelta.day(1).
+
+            to_date (datetime.date, optional): 
+                To date of the historical data. 
+                Defaults to datetime.date.today().
+
+            country ([str, optional): 
+                Coubtry of the issued security. 
+                Mandatory only for 'stock' and 'fund'. 
+                Defaults to None.
+
+        Raises:
+            ValueError: [description]
+
+        Returns:
+            str: prepared query for investpy
+        """
+        q_historical_data = {
+                'stock'         : "investpy.get_stock_historical_data(\"{security}\", country= \"{country}\", from_date= \"{from_date}\", to_date= \"{to_date}\")",
+                'fund'          : 'investpy.get_fund_historical_data(\"{security}\", country= \"{country}\", from_date= \"{from_date}\", to_date= \"{to_date}\")',
+                'currency_cross': 'investpy.get_currency_cross_historical_data(\"{security}\", from_date= \"{from_date}\", to_date= \"{to_date}\")',
+            }
+        if type(security_type) is not str or type(security) is not str:
+            raise TypeError('arg not of type str.')
+
+        elif country != None and type(country) is not str:
+            raise TypeError('arg not of type str.')
+
+        elif type(from_date) is not datetime.date:
+            raise TypeError('dates not of type datetime.time')
+
+        elif security_type not in q_historical_data.keys():
+            raise ValueError('security type not supported.')
+
+        else:
+            if security_type in ['stock', 'fund']:
+                return q_historical_data[security_type.lower()].format(security=security.upper(), country=country, from_date=self.__datetime_to_string(from_date), to_date=self.__datetime_to_string(to_date))
+            else:
+                return q_historical_data[security_type.lower()].format(security=security.upper(), from_date=self.__datetime_to_string(from_date), to_date=self.__datetime_to_string(to_date))
+
+
+        
+    def __datetime_to_string(self, date_value):
+        """Converts datetime.date to str of format DD/MM/YYYY.
+
+        Args:
+            date_value (datetime.date): date
+
+        Returns:
+            str: converted date
+        """
+        if type(date_value) not in [datetime.date, datetime.datetime]:
+            raise TypeError('Not a datetime')
+        else:
+            date_format = "{day}/{month}/{year}"
+            return date_format.format(day=date_value.day, month=date_value.month, year=date_value.year)
--- portfolio/tests/test_security.py
+++ portfolio/tests/test_security.py
@@ -1,5 +1,6 @@
 import unittest
 import datetime
+from time import sleep 
 
 from portfolio.classes.securities import Security
 
@@ -71,7 +72,7 @@ class TestMethod_get_search_security_query_investpy(unittest.TestCase):
         self.assertEqual(self.security.get_search_security_query_investpy('currency_Cross', 'USD/sek'), self.expected_outputs[3])
         self.assertEqual(self.security.get_search_security_query_investpy('CURRENCY_CROSS', 'usd/sek'), self.expected_outputs[3])
         self.assertEqual(self.security.get_search_security_query_investpy('currency_cross', 'USD/SEK'), self.expected_outputs[3])
-        
+
 class TestMethod_get_basic_info(unittest.TestCase):
     def setUp(self):
         # test stock function on Evolution Gaming Group AB
@@ -148,4 +149,180 @@ class TestMethod_get_basic_info(unittest.TestCase):
         self.assertEqual(results_currency_cross['currency']      , self.currency_cross_data['currency'])
 
 
+class TestMethod_get_historical_data_query_investpy(unittest.TestCase):
+    def setUp(self):
+        self.security = Security('SE0012673267')
+        self.expected_outputs = {
+            1: 'investpy.get_stock_historical_data(\"EVOG\", country= \"sweden\", from_date= \"1/1/2021\", to_date= \"31/12/2021\")',
+            2: 'investpy.get_fund_historical_data(\"CORE NY TEKNIK A\", country= \"sweden\", from_date= \"1/1/2021\", to_date= \"31/12/2021\")',
+            3: 'investpy.get_currency_cross_historical_data(\"USD/SEK\", from_date= \"1/1/2021\", to_date= \"31/12/2021\")'
+        }
+        self.from_date = datetime.date(year=2021, month=1,  day=1)
+        self.to_date   = datetime.date(year=2021, month=12, day=31)
 
+    def test_type_error_raise(self):
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, None,    'EVOG', 'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 1,       'EVOG', 'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, list(),  'EVOG', 'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, -1.0,    'EVOG', 'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 5j,      'EVOG', 'sweden', self.from_date, self.to_date)
+
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', None,   'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 1,      'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', list(), 'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', -1.0,   'sweden', self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 5j,     'sweden', self.from_date, self.to_date)
+
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 1,        self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', list(),   self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', -1.0,     self.from_date, self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 5j,       self.from_date, self.to_date)
+
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', 'abc',          self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', 1,              self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', list(),         self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', -1.0,           self.to_date)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', 5j,             self.to_date)
+
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', self.from_date, 'abc')
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', self.from_date, 1)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', self.from_date, list())
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', self.from_date, -1.0)
+        self.assertRaises(TypeError, self.security.get_historical_data_query_investpy, 'stock', 'EVOG', 'sweden', self.from_date, 5j)
+
+        
+
+    def test_value_error_raise(self):
+        self.assertRaises(ValueError, self.security.get_historical_data_query_investpy, 'abc',  'EVOG', 'sweden', self.from_date, self.to_date)
+    
+    def test_output(self):
+        self.assertEqual(self.security.get_historical_data_query_investpy('stock',          'EVOG',                 'sweden',   self.from_date, self.to_date), self.expected_outputs[1])
+        self.assertEqual(self.security.get_historical_data_query_investpy('fund',           'Core Ny Teknik A',     'sweden',   self.from_date, self.to_date), self.expected_outputs[2])
+        self.assertEqual(self.security.get_historical_data_query_investpy('currency_cross', 'USD/SEK',              None,       self.from_date, self.to_date), self.expected_outputs[3])
+
+
+class TestMethod_get_security_price(unittest.TestCase):
+    def setUp(self):
+        self.from_date = datetime.date(year=2021, month=1, day=1)
+        self.to_date = datetime.date(year=2021, month=1, day=5)
+
+        self.securties = [
+            Security('SE0012673267'),
+            Security('SE0012193019'),
+            Security('USD/SEK')
+        ]
+        
+        self.random_security_prices = {
+            'Evolution Gaming': {
+                datetime.date(year=2018, month=6,  day=8):  113.0,
+                # datetime.date(year=2020, month=2,  day=24): 407.0,
+                # datetime.date(year=2020, month=10, day=27): 690.0,
+                # datetime.date(year=2021, month=4,  day=29): 1666.2,
+                datetime.date(year=2021, month=12,  day=3): 935.0
+            },
+            'Core Ny Teknik A': {
+                datetime.date(year=2020, month=2,  day=24): 140.930,
+                # datetime.date(year=2020, month=10, day=27): 187.614,
+                # datetime.date(year=2021, month=4,  day=29): 236.883,
+                datetime.date(year=2021, month=12,  day=3): 219.191
+            },
+            'USD/SEK': {
+                datetime.date(year=2018, month=6,  day=8):  8.7081,
+                # datetime.date(year=2020, month=2,  day=24): 9.7319,
+                # datetime.date(year=2020, month=10, day=27): 8.7226,
+                # datetime.date(year=2021, month=4,  day=29): 8.3705,
+                datetime.date(year=2021, month=12,  day=3): 9.1364
+            }
+            
+        }
+        self.security_price_range_short = {
+            'Evolution Gaming': {
+                datetime.date(year=2022, month=1, day=3): 1282.8,
+                datetime.date(year=2022, month=1, day=4): 1282.0,
+                datetime.date(year=2022, month=1, day=5): 1284.8
+            },
+            'Core Ny Teknik A': {
+                datetime.date(year=2022, month=1, day=3): 233.817,
+                datetime.date(year=2022, month=1, day=4): 231.458,
+                datetime.date(year=2022, month=1, day=5): 229.064
+            },
+            'USD/SEK': {
+                datetime.date(year=2022, month=1, day=3): 9.1033,
+                datetime.date(year=2022, month=1, day=4): 9.0871,
+                datetime.date(year=2022, month=1, day=5): 9.1041
+            }
+        }
+        self.security_price_range_long = {
+            'Evolution Gaming': {
+                datetime.date(year=2022, month=1, day=1): 1286.2,
+                datetime.date(year=2022, month=1, day=2): 1286.2,
+                datetime.date(year=2022, month=1, day=3): 1282.8,
+                datetime.date(year=2022, month=1, day=4): 1280.0,
+                datetime.date(year=2022, month=1, day=5): 1284.8
+            },
+            'Core Ny Teknik A': {
+                datetime.date(year=2022, month=1, day=1): 233.817,
+                datetime.date(year=2022, month=1, day=2): 233.817,
+                datetime.date(year=2022, month=1, day=3): 233.817,
+                datetime.date(year=2022, month=1, day=4): 231.458,
+                datetime.date(year=2022, month=1, day=5): 229.064
+            },
+            'USD/SEK': {
+                datetime.date(year=2022, month=1, day=1): 9.0392,
+                datetime.date(year=2022, month=1, day=2): 9.0392,
+                datetime.date(year=2022, month=1, day=3): 9.1033,
+                datetime.date(year=2022, month=1, day=4): 9.0871,
+                datetime.date(year=2022, month=1, day=5): 9.1041
+            }   
+        }
+
+    def test_type(self):
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), None,      datetime.date(year=2021, month=5, day=5))
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), 'abc',     datetime.date(year=2021, month=5, day=5))
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), 1,         datetime.date(year=2021, month=5, day=5))
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), list(),    datetime.date(year=2021, month=5, day=5))
+
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), datetime.date(year=2021, month=5, day=5), None)
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), datetime.date(year=2021, month=5, day=5), 'abc')
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), datetime.date(year=2021, month=5, day=5), 1)
+        self.assertRaises(TypeError, self.securties[0].get_security_price(), datetime.date(year=2021, month=5, day=5), list())
+
+
+    def test_get_security_price_output(self):
+        msg = '{security}s stock price don\'t match at date {date}'
+
+        for security in self.securties:
+            for date in list(self.random_security_prices[security.get_basic_info()['name']].keys()):
+                # Time delay to avoid error code 429 
+                # by breaching query limit of investing.com and being blacklisted for 24h
+                # https://github.com/alvarobartt/investpy/issues/467
+                sleep(5)
+
+                # test whether output is of length 1, i.e. stock price for a given day
+                self.assertEqual(len(list(security.get_security_price(date).keys())), 1, msg='output is not for single day')
+
+                # test whether stock price matches raw data
+                self.assertEqual(
+                    security.get_security_price(date)[date], 
+                    self.random_security_prices[security.get_basic_info()['name']][date], 
+                    msg=msg.format(security= security.get_basic_info()['name'], date= date)
+                )
+
+        
+
+    def test_get_security_historical_price_output(self):
+        msg = '{security}s stock price don\'t match at date {date}'
+
+        for security in self.securties:
+            for date in list(self.security_price_range_short[security.get_basic_info()['name']].keys()):
+                # Time delay to avoid error code 429 
+                # by breaching query limit of investing.com and being blacklisted for 24h
+                # https://github.com/alvarobartt/investpy/issues/467
+                sleep(5)
+
+                # test whether stock price matches raw data
+                self.assertEqual(
+                    security.get_security_price(date)[date], 
+                    self.security_price_range_short[security.get_basic_info()['name']][date], 
+                    msg=msg.format(security= security.get_basic_info()['name'], date= date)
+                )
